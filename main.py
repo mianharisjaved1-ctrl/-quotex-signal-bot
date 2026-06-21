a2329acf607ea72eaa9f5cdb5c69150fb7b41d3d
mianharisjaved1-ctrl <mianharisjaved1@gmail.com>
2026-06-22 03:20:33 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,15 @@
-import time
 import telebot
-import random
+import time
 
-TOKEN ="8671909594:AAETOOdgQ8XfW9UiAUpIUR5RFjIFpi97c-w"
-CHAT_ID ="7036550926"
+TOKEN = "8671909594:AAGzbMdw9SCbk2DP16MabrR_6u4PVc1gjyI"
+CHAT_ID = "7036550926"
 
 bot = telebot.TeleBot(TOKEN)
 
-def signal():
-    rsi = random.randint(30, 70)
-    trend = random.choice(["up", "down"])
-
-    if trend == "up" and rsi > 55:
-        return "📈 BUY"
-    elif trend == "down" and rsi < 45:
-        return "📉 SELL"
-    else:
-        return "⏸ NO TRADE"
-
 while True:
-    msg = signal()
-    bot.send_message(CHAT_ID, f"Quotex Signal:\n{msg}")
-    time.sleep(60)
+    try:
+        bot.send_message(CHAT_ID, "🤖 Bot is working!")
+        time.sleep(60)
+    except Exception as e:
+        print(e)
+        time.sleep(10)
